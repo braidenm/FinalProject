76dae5eca2ce6a144e318093a35041d02c79d4c0
Braiden Miller <braidenjamesmiller@gmail.com>
2019-02-13 13:29:57 -0700

    added rest and readme

---
+++ doggyTinderRest/src/main/java/com/skilldistillery/doggyTinder/DoggyTinderRestApplication.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.doggyTinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggyTinderRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoggyTinderRestApplication.class, args);
+	}
+
+}
+

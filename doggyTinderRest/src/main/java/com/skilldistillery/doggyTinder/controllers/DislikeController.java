4ba69645d3d09d76c0951f50bd5274448b293f52
roguetycoon <michaelmurrell49@gmail.com>
2019-02-15 15:58:58 -0700

    dislike controller

---
+++ doggyTinderRest/src/main/java/com/skilldistillery/doggyTinder/controllers/DislikeController.java
@@ -0,0 +1,54 @@
+package com.skilldistillery.doggyTinder.controllers;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.doggyTinder.entities.Dislike;
+import com.skilldistillery.doggyTinder.entities.Dog;
+import com.skilldistillery.doggyTinder.services.DislikeService;
+
+@RestController
+@RequestMapping("api")
+@CrossOrigin({ "*", "http://localhost:4201" })
+public class DislikeController {
+
+	@Autowired
+	private DislikeService dServ;
+
+	@PostMapping("dogs/dislikes/{id1}/{id2}")
+	public Dog addDislike(HttpServletResponse res, @PathVariable Integer id1, @PathVariable Integer id2) {
+
+		try {
+			res.setStatus(201);
+			return dServ.addDislike(id1, id2);
+		} catch (Exception e) {
+			e.printStackTrace();
+			res.setStatus(404);
+			return null;
+
+		}
+
+	}
+	@PostMapping("dogs/dislikes/{id1}")
+	public List<Dislike> getAllDislikes(HttpServletResponse res, @PathVariable Integer id1) {
+		
+		try {
+			res.setStatus(201);
+			return dServ.getAllDislikes(id1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			res.setStatus(404);
+			return null;
+			
+		}
+		
+	}
+}

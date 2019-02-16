24486b5343b4a1de59bdb1cbd41304dbf69f382d
roguetycoon <michaelmurrell49@gmail.com>
2019-02-16 13:11:30 -0700

    Match Controller completed

--- doggyTinderRest/src/main/java/com/skilldistillery/doggyTinder/repositories/MatchRepo.java
+++ doggyTinderRest/src/main/java/com/skilldistillery/doggyTinder/repositories/MatchRepo.java
@@ -4,11 +4,11 @@ import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-import com.skilldistillery.doggyTinder.entities.Dog;
+import com.fasterxml.jackson.databind.deser.DataFormatReaders.Match;
 import com.skilldistillery.doggyTinder.entities.Matches;
 
 public interface MatchRepo extends JpaRepository<Matches, Integer> {
 
 	Set<Matches> findByThisDog_idOrThatDog_id(Integer id, Integer id2);
+	Matches findByThisDog_idAndThatDog_id(Integer id, Integer id2);
 }
--- doggyTinderRest/src/main/java/com/skilldistillery/doggyTinder/services/MatchServiceImpl.java
+++ doggyTinderRest/src/main/java/com/skilldistillery/doggyTinder/services/MatchServiceImpl.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.skilldistillery.doggyTinder.entities.Dislike;
+import com.fasterxml.jackson.databind.deser.DataFormatReaders.Match;
 import com.skilldistillery.doggyTinder.entities.Dog;
 import com.skilldistillery.doggyTinder.entities.Matches;
 import com.skilldistillery.doggyTinder.repositories.DogRepo;
@@ -22,15 +22,24 @@ public class MatchServiceImpl implements MatchService {
 	
 	@Override
 	public Dog addMatch(Integer thisDog, Integer thatDog) {
+		
 		Optional<Dog> op = dogRepo.findById(thisDog);
 		Optional<Dog> op2 = dogRepo.findById(thatDog);
 		Matches match = new Matches();
 		if(op.isPresent() && op2.isPresent()) {
+			Dog dog = op.get();
+			Dog dog2 = op2.get();
+			for (Matches matches: mRepo.findAll()) {
+				if (matches.getThisDog().equals(dog) && matches.getThatDog().equals(dog2)) {
+					return null;
+				}
+				if(matches.getThisDog().equals(dog2) && matches.getThatDog().equals(dog)) {
+					return null;
+				}
+			}
 			match.setThatDog(op2.get());
 			match.setThisDog(op.get());
 			mRepo.saveAndFlush(match);
-			Dog dog = op.get();
-			Dog dog2 = op2.get();
 			dog.addMatch(match);
 			dog2.addMatch(match);
 			dogRepo.saveAndFlush(dog);
@@ -44,11 +53,10 @@ public class MatchServiceImpl implements MatchService {
 	public Dog deleteMatch(Integer thisDog, Integer thatDog) {
 		Optional<Dog> op = dogRepo.findById(thisDog);
 		Optional<Dog> op2 = dogRepo.findById(thatDog);
-		Matches match = new Matches();
+		Matches match =  mRepo.findByThisDog_idAndThatDog_id(thisDog, thatDog);
 		if(op.isPresent() && op2.isPresent()) {
 			match.setThatDog(null);
 			match.setThisDog(null);
-			
 			Dog dog = op.get();
 			Dog dog2 = op2.get();
 			dog.removeMatch(match);

a190c4fe7546d0aacd22c8c2e6f7dc6818112f2d
roguetycoon <michaelmurrell49@gmail.com>
2019-02-16 13:50:35 -0700

    dislike ngModel completed

--- ngDoggyTinder/src/app/models/dis-like.ts
+++ ngDoggyTinder/src/app/models/dis-like.ts
@@ -1,4 +1,14 @@
+import { Dog } from './dog';
+
 export class DisLike {
 
+  id: number;
+  thisDog: Dog;
+  thatDog: Dog;
 
+  constructor(id: number, thisDog: Dog, thatDog:Dog){
+    this.id = id;
+    this.thisDog = thisDog;
+    this.thatDog = thatDog;
+  }
 }

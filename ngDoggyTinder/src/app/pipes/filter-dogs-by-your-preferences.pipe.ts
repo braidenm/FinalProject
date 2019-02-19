a79cfc9ec402304f5b54e53a2a4a9fae92d3fd3a
Jimmy Fruchterman <jdfruchterman@gmail.com>
2019-02-18 17:26:19 -0700

    filtering pipes written

--- ngDoggyTinder/src/app/pipes/filter-dogs-by-your-preferences.pipe.ts
+++ ngDoggyTinder/src/app/pipes/filter-dogs-by-your-preferences.pipe.ts
@@ -1,3 +1,4 @@
+import { Dog } from 'src/app/models/dog';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
@@ -5,8 +6,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterDogsByYourPreferencesPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(dogs: Dog[], selectedDog: Dog): any {
+    const results = [];
+
+    dogs.forEach((dog) => {
+      if (dog.age >= selectedDog.Preferences.minAge && dog.age <= selectedDog.Preferences.maxAge) {
+        if (dog.weight >= selectedDog.Preferences.minWeight && dog.weight <= selectedDog.Preferences.maxWeight) {
+          if (dog.energy >= selectedDog.Preferences.minEnergy && dog.energy <= selectedDog.Preferences.maxEnergy) {
+        results.push(dog);
+          }
+        }
+      }
+    });
+    return results;
   }
 
 }

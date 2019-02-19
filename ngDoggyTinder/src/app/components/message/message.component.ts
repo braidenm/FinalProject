2d9e118a1aca129ba5d1922b2baa5caaa4c87b09
Braiden Miller <braidenjamesmiller@gmail.com>
2019-02-19 11:30:32 -0700

    sync

--- ngDoggyTinder/src/app/components/message/message.component.ts
+++ ngDoggyTinder/src/app/components/message/message.component.ts
@@ -1,8 +1,11 @@
+import { MatchService } from './../../services/match.service';
+import { MatchService } from 'src/app/services/match.service';
 import { MessageService } from './../../services/message.service';
 import { Component, OnInit } from '@angular/core';
 import { Dog } from 'src/app/models/dog';
 import { DogService } from 'src/app/services/dog.service';
 import { Message } from 'src/app/models/message';
+import { ifStmt } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-message',
@@ -14,14 +17,30 @@ export class MessageComponent implements OnInit {
   thatDog: Dog;
   convo: Message[] = [];
   text = '';
+  isMatch = false;
 
-  constructor(private messageS: MessageService, dogServe: DogService) { }
+  constructor(private messageS: MessageService, private dogServe: DogService, private matchServe: MatchService) { }
 
   ngOnInit() {
     this.thisDog = this.messageS.getThisDog();
     this.thatDog = this.messageS.getThatDog();
+    console.log(this.thisDog);
+    console.log(this.thatDog);
     this.getConversation();
 
+  }
+
+  checkIfMatch() {
+    this.matchServe.index(this.thisDog.id).subscribe(
+      data => {
+        for (const match of data) {
+          if (this.thatDog.id === match.thatDog.id) {
+            this.isMatch = true;
+            break;
+          }
+        }
+      }
+    );
   }
 
   getConversation() {

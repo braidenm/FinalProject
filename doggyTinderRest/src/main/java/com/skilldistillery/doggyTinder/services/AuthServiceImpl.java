d84ef7f6b0ae192c9a7aadc50fc32a6a92888182
roguetycoon <michaelmurrell49@gmail.com>
2019-02-20 19:01:48 -0700

    what

--- doggyTinderRest/src/main/java/com/skilldistillery/doggyTinder/services/AuthServiceImpl.java
+++ doggyTinderRest/src/main/java/com/skilldistillery/doggyTinder/services/AuthServiceImpl.java
@@ -6,33 +6,30 @@ import org.springframework.stereotype.Service;
 
 import com.skilldistillery.doggyTinder.entities.User;
 import com.skilldistillery.doggyTinder.repositories.AddressRepo;
-import com.skilldistillery.doggyTinder.repositories.DogRepo;
 import com.skilldistillery.doggyTinder.repositories.UserRepo;
+
+
 @Service
 public class AuthServiceImpl implements AuthService {
-	
-	@Autowired
-	private PasswordEncoder pwEncoder;
-	
-	@Autowired
-	private UserRepo uRepo;
-	@Autowired
-	private AddressRepo aRepo;
-	@Autowired
-	private DogRepo dRepo;
-	
-	
-	
-	@Override
-	public User register(User user) {
-		user.setPassword(pwEncoder.encode(user.getPassword()));
-		user.setActive(true);
-		user.setRole("standard");
-		user.setBanned(false);
-		aRepo.saveAndFlush(user.getAddress());
-		
-		uRepo.saveAndFlush(user);
-		return user;
-	}
+    
+    @Autowired
+    private PasswordEncoder pwEncoder;
+    
+    @Autowired
+    private UserRepo uRepo;
+    @Autowired
+    private AddressRepo aRepo;
+    
+    @Override
+    public User register(User user) {
+        user.setPassword(pwEncoder.encode(user.getPassword()));
+        user.setActive(true);
+        user.setRole("standard");
+        user.setBanned(false);
+        aRepo.saveAndFlush(user.getAddress());
+        
+        uRepo.saveAndFlush(user);
+        return user;
+    }
 
 }
